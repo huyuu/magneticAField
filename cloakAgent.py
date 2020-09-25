b809995c598e4256e01cb8dce92054bea894e261
yuyang <huyuu258@gmail.com>
2020-09-25 17:12:15 +0900

    5 update

--- cloakAgent.py
+++ cloakAgent.py
@@ -118,6 +118,7 @@ class CloakAgent():
             collectedAmount += 1
         _end = dt.datetime.now()
         print('All {} trajectories generated. (cost {:.3g} hours)'.format(_amount, (_end-_start).total_seconds()/3600.0))
+        print(bs)
         # sort before save
         bs = bs.sort(axis=0)
         # save results
